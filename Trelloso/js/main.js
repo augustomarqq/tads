ef53bf5a143b1726f9671baca2f3e1e3cf54b996
Augusto Marques <108703827+augustomarqq@users.noreply.github.com>
2024-01-12 02:21:29 -0300

    Fluxo de criação de boards finalizado

--- Trelloso/js/main.js
+++ Trelloso/js/main.js
@@ -1,6 +1,11 @@
 import User from "./users.js";
 import Token from "./token.js";
+import Board from "./boards.js";
 
+const classesToToggle = document.querySelectorAll(
+  ".show-content, .hide-content"
+);
+let forms = document.getElementById("forms");
 let login = document.getElementById("login");
 let formLogin = document.getElementById("form-login");
 let formCreateUser = document.getElementById("form-create-user");
@@ -9,15 +14,20 @@ let listUsers = document.getElementById("list-users");
 let btnListUsers = document.getElementById("btn-users");
 let spanMe = document.getElementById("me");
 let sidebar = document.getElementById("sidebar");
-let headerMenu = document.getElementById("header-menu");
+let headerMenu = document.getElementById("home-header");
 let homeIntro = document.getElementById("home-intro");
 let homeSair = document.getElementById("home-sair");
 let refCreateAcc = document.getElementById("ref-create-acc");
 let refLogin = document.getElementById("ref-fazer-login");
 let mainContainer = document.getElementById("main-container");
-const classesToToggle = document.querySelectorAll(
-  ".show-content, .hide-content"
-);
+let menuUsers = document.getElementById("menu-users");
+let users = document.getElementById("users");
+let boardsContent = document.getElementById("boards-content");
+let homeHome = document.getElementById("home-home");
+let boards = document.getElementById("menu-boards");
+let listBoards = document.getElementById("list-boards");
+let createBoard = document.getElementById("create-new-board");
+let formCreateBoard = document.getElementById("form-board");
 
 formLogin.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -53,7 +63,26 @@ formLogin.addEventListener("submit", (event) => {
   });
 });
 
+menuUsers.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  forms.classList.remove("hide-content");
+  forms.classList.add("show-content-grid");
+
+  classesToToggle.forEach((element) => {
+    if (element === users || element === sidebar || element === headerMenu) {
+      element.classList.remove("hide-content");
+      element.classList.add("show-content");
+    } else {
+      element.classList.remove("show-content");
+      element.classList.add("hide-content");
+    }
+  });
+});
+
 btnListUsers.addEventListener("click", (event) => {
+  event.preventDefault();
+
   listUsers.innerHTML = "";
   User.getAll().then((users) => {
     for (let user of users) {
@@ -127,9 +156,11 @@ homeSair.addEventListener("click", (event) => {
 
   mainContainer.classList.remove("logged-in");
   mainContainer.classList.add("logged-off");
+  forms.classList.remove("hide-content");
+  forms.classList.add("show-content-grid");
 
   classesToToggle.forEach((element) => {
-    if (element === login) {
+    if (element === forms || element === login) {
       element.classList.remove("hide-content");
       element.classList.add("show-content");
     } else {
@@ -137,4 +168,103 @@ homeSair.addEventListener("click", (event) => {
       element.classList.add("hide-content");
     }
   });
+});
+
+boards.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  listBoards.innerHTML = "";
+  Board.myBoards().then((boards) => {
+    for (let board of boards) {
+      const li = document.createElement("li");
+      li.innerHTML = board.name;
+      li.style.backgroundColor = board.color;
+      const starContainer = document.createElement("div");
+      starContainer.id = "fav-star";
+      starContainer.style.backgroundColor = board.color;
+      const starIcon = document.createElement("i");
+      starIcon.className = "fa-regular fa-star";
+      starContainer.appendChild(starIcon);
+      li.appendChild(starContainer);
+      listBoards.appendChild(li);
+    }
+  });
+
+  forms.classList.remove("show-content-grid");
+  forms.classList.add("hide-content");
+
+  classesToToggle.forEach((element) => {
+    if (
+      element === boardsContent ||
+      element === sidebar ||
+      element === headerMenu
+    ) {
+      element.classList.remove("hide-content");
+      element.classList.add("show-content");
+    } else {
+      element.classList.remove("show-content");
+      element.classList.add("hide-content");
+    }
+  });
+});
+
+homeHome.addEventListener("onclick", (event) => {
+  event.preventDefault();
+
+  listBoards.innerHTML = "";
+  Board.myBoards().then((boards) => {
+    for (let board of boards) {
+      const li = document.createElement("li");
+      li.innerHTML = board.boardName;
+      listBoards.appendChild(li);
+    }
+  });
+
+  forms.classList.remove("show-content-grid");
+  forms.classList.add("hide-content");
+
+  classesToToggle.forEach((element) => {
+    if (
+      element === boardsContent ||
+      element === sidebar ||
+      element === headerMenu
+    ) {
+      element.classList.remove("hide-content");
+      element.classList.add("show-content");
+    } else {
+      element.classList.remove("show-content");
+      element.classList.add("hide-content");
+    }
+  });
+});
+
+formCreateBoard.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const boardName = document.getElementById("new-title").value;
+  const boardBackground = document.getElementById("color-dropdown").value;
+
+  console.log(boardName, boardBackground);
+  Board.create(boardName, boardBackground)
+    .then((board) => {
+      console.log(board);
+      alert("Quadro novo criado com sucesso!");
+      formCreateBoard.reset();
+
+      const li = document.createElement("li");
+      li.innerHTML = board.name;
+      li.style.backgroundColor = board.color;
+      const starContainer = document.createElement("div");
+      starContainer.id = "fav-star";
+      starContainer.style.backgroundColor = board.color;
+      const starIcon = document.createElement("i");
+      starIcon.className = "fa-regular fa-star";
+      starContainer.appendChild(starIcon);
+      li.appendChild(starContainer);
+      listBoards.appendChild(li);
+    })
+    .catch((error) => {
+      console.log(error.message);
+      alert("Erro ao criar quadro. Por favor, tente novamente.");
+    });
 });
